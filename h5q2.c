d803dc98f37aa9c20ea08fa7544f125b9b32380c
Ori Tzoran <ori.tzoran@icloud.com>
2018-06-23 18:27:52 +0300

    not working scanf, next: replace it

--- h5q2.c
+++ h5q2.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int input_val(char *p) ;
 
 int main()
 {
-	int		coin_face_values[];
+	int		*coin_face_values;
 	int		num_of_coins;
 	int		sum_of_coins;
 	int		max_coins_to_use;
@@ -10,34 +13,31 @@ int main()
 	int		number_of_combinations = 0;
 
     
-    printf("Enter number of coins to use: ");
-		// make func to grab int input
-	if (scanf("%d ", &num_of_coins) != 1) {
-		printf("error scanf\n");
-		exit(1);
-	}
-
-    printf("Enter sum of coins: ");
-		// make func to grab int input
-	if (scanf("%d ", &sum_of_coins) != 1) {
-		printf("error scanf\n");
-		exit(1);
-	}
-
-	// same func to grab max_coins_to_use
-	
+	num_of_coins = input_val("Enter number of coins to use: ");
+	sum_of_coins = input_val("Enter sum of coins: ");
+	max_coins_to_use = input_val("Enter how many coins max: ");
 
 	coin_face_values = malloc(sizeof(num_of_coins) * num_of_coins);
 	for (ii = 0; ii < num_of_coins; ii++) {
-		printf("Enter face value of coin %d: ", ii);
-		// check no dups
-		if (scanf("%d ", coin_face_values[ii]) != 1) {
-			printf("error scanf\n");
-			exit(1);
-		}
+		printf("[coin[%d] ", ii);
+		coin_face_values[ii] = input_val("Enter face value of coin: ");
+		// check no dups4
 	}
 
 	printf("number_of_combinations of found: %d\n", number_of_combinations);
 
     return 0;
+}
+
+int input_val(char *p) {
+	int		val, ret;
+
+	// make func to grab int input
+    printf("%s", p);
+	ret = scanf("%d\n", &val);
+	if (ret == 1) 
+		return val;
+	// err
+	printf("error scanf\n");
+	exit(1);
 }
